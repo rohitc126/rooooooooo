250986cc2dab8d687f4cdc232a5f9b2b95f17c27
rohitc126 <121222869+rohitc126@users.noreply.github.com>
2023-05-27 18:53:01 +0530

    Add files via upload

---
+++ BalanceSheetData.aspx
@@ -0,0 +1,127 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FA_Master.master" AutoEventWireup="true" CodeFile="BalanceSheetData.aspx.cs" Inherits="FAMS_Master_BalanceSheetData" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <link href="../../css/FA_1.css" rel="stylesheet" type="text/css" />
+    <script src="../../Jquery/jquery-1.4.2.min.js" type="text/javascript"></script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+ <table  width="100%" style="overflow: scroll;">
+  <tr>
+    <td colspan="7">
+                <asp:Panel ID="ErrorContainer" runat="server">
+                    <asp:Label ID="MyMessage" runat="server" Font-Size="10pt"></asp:Label>
+                </asp:Panel>
+            </td>
+            
+            </tr>
+            <tr>
+           <td width="10%">&nbsp;</td>
+              <td class="Left" width="10%">
+                Company:
+            </td> 
+            <td class="Left" style="width: 25%">
+                <asp:DropDownList ID="DDLCompanyName" runat="server" Style="width: 200px" CssClass="ddlNormal"
+                    AutoPostBack="true">
+                    <asp:ListItem Text="Select Company" Value="0"></asp:ListItem>
+                     
+                </asp:DropDownList>
+                <asp:RequiredFieldValidator ID="rfv_ddlCompanyshow" runat="server" ValidationGroup="Show1"
+                    Display="Dynamic" ControlToValidate="DDLCompanyName" ErrorMessage="Select Company Name"
+                    ToolTip="Select Company Name" SetFocusOnError="True" InitialValue="0">*</asp:RequiredFieldValidator>
+            </td>
+
+             <td class="Left" width="10%">
+               Financial Year
+            </td>
+            <td class="Left" width="25%">
+                <asp:DropDownList ID="ddlFinanceYear" runat="server" Style="width: 220px" CssClass="ddlNormal"
+                   AutoPostBack="True" 
+                    onselectedindexchanged="ddlFinanceYear_SelectedIndexChanged">
+
+                    <asp:ListItem  Text="Select Finance Year" Value="0"></asp:ListItem>
+                </asp:DropDownList>
+                <asp:RequiredFieldValidator ID="rfv_ddlFinanceYear" runat="server" ValidationGroup="Show1"
+                    Display="Dynamic" ControlToValidate="ddlFinanceYear" ErrorMessage="Select Finance Year"
+                    ToolTip="Select Finance Year" SetFocusOnError="True" InitialValue="0">*</asp:RequiredFieldValidator>
+            </td>
+             <td width="20%">&nbsp;</td>
+            </tr>
+            </table>
+            <table id="gridheader" runat="server">
+             <tr>
+            <td class="con_header" width="20%">
+               Balance Sheet Data: 2 to 3 Notes
+            </td>
+            <td colspan="3" width="80%">
+            </td>
+        </tr>
+                      <tr>
+                        <td colspan="4">
+                               <asp:GridView ID="GVBalance" runat="server" AutoGenerateColumns="false" 
+                                Width="80%" CssClass="grid-view_1" EmptyDataText="Record Not Found" EnableTheming="True"
+                                ShowHeader="true" CellPadding="4" CellSpacing="0"  PageSize="10" PagerStyle-ForeColor="#006600" 
+                               onrowdatabound="GVBalance_RowDataBound">
+                                <HeaderStyle CssClass="header_1" />
+                                <RowStyle CssClass="normal_1" />
+                                <FooterStyle CssClass="header_1" />
+                                <AlternatingRowStyle CssClass="alternaterow" />
+                              
+                                <Columns>
+                                    <asp:TemplateField HeaderText="BS_ID" >
+                                        <ItemTemplate>
+                                         <asp:Label ID="lblSrNo" runat="server" Text='<%#Container.DisplayIndex+1 %>'></asp:Label>
+                                            <asp:Label ID="lblBS_ID" runat="server" Visible="false" Text='<%#Eval("bs_Id")%>'></asp:Label>
+                                              <asp:HiddenField ID="hdnbsid" runat="server" Value ='<%#Eval("bs_Id")%>'></asp:HiddenField>
+                                            <asp:HiddenField ID="hdnAction" runat="server" ></asp:HiddenField>
+                                        </ItemTemplate>
+                                        <ItemStyle HorizontalAlign="Center" Width="5%" />
+                                    </asp:TemplateField>
+                                    <asp:TemplateField HeaderText="Note">
+                                        <ItemTemplate>
+                                            <asp:Label ID="lblNote" runat="server" Text='<%#Eval("NOTE")%>'></asp:Label>
+                                        </ItemTemplate>
+                                        <ItemStyle HorizontalAlign="Left" Width="10%" />
+                                    </asp:TemplateField>
+                                      <asp:TemplateField HeaderText="Item">
+                                        <ItemTemplate>
+                                            <asp:Label ID="lblItem" runat="server" Text='<%#Eval("Items")%>'></asp:Label>
+                                        </ItemTemplate>
+                                        <ItemStyle HorizontalAlign="Left" Width="30%" />
+                                    </asp:TemplateField>
+
+                                    <asp:TemplateField HeaderText="QTY">
+                                        <ItemTemplate>
+                                            <asp:TextBox ID="txtQTY" runat="server" Text='<%#Eval("QTY")%>'></asp:TextBox>
+                                            <asp:HiddenField ID="hdnQTY" runat="server" Value='<%#Eval("QTY")%>'  />
+                                        </ItemTemplate>
+                                        <ItemStyle HorizontalAlign="Center" Width="10%" />
+                                    </asp:TemplateField>
+                                    <asp:TemplateField HeaderText="RATE" >
+                                        
+                                        <ItemTemplate>
+                                            <asp:TextBox ID="txtRATE" runat="server" Text='<%#Eval("RATE")%>'></asp:TextBox>
+                                             <asp:HiddenField ID="hdnRATE" runat="server" Value='<%#Eval("RATE")%>'  />
+                                        </ItemTemplate>
+                                        <ItemStyle HorizontalAlign="Right" Width="10%" />
+                                    </asp:TemplateField>
+                                </Columns>
+                            </asp:GridView>
+                        </td>
+                    </tr>
+             <tr>
+            <td colspan="4" align="center">
+                <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="Submit" Width="10%" CausesValidation="true"
+                    ValidationGroup="vin" onclick="btnSave_Click1"  />
+             <%--   <asp:Button ID="btnUpdate" runat="server" Text="Update" CssClass="Submit" 
+                    CausesValidation="false" onclick="btnUpdate_Click1" />--%>
+                <asp:ValidationSummary ID="ValidationSummary2" ValidationGroup="vin" ShowMessageBox="true"
+                    ShowSummary="false" runat="server"/>
+           
+
+            </td>
+        </tr>
+ </table>
+</asp:Content>
+
+
---
+++ BalanceSheetData.aspx.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class FAMS_Master_BalanceSheetData : System.Web.UI.Page
+{
+    BAL_EmployeeLevelAccess EmployeeLevelBAL = new BAL_EmployeeLevelAccess();
+    Message msg = new Message();
+    BAL_FA_LedgerMapping Notes = new BAL_FA_LedgerMapping();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            fill_company();
+            fill_Finance_year();
+            btnSave.Visible = false;
+            gridheader.Visible = false;
+           
+        }
+    }
+
+
+    protected void fill_company()
+    {
+        DDLCompanyName.Items.Clear();
+        DataTable dtCompany = EmployeeLevelBAL.LoadEmployeeCompanyAccess(Session["LogIn_Code"].ToString());
+        if (dtCompany.Rows.Count > 0)
+        {
+            DDLCompanyName.DataSource = dtCompany;
+            DDLCompanyName.DataTextField = "Comp_Name";
+            DDLCompanyName.DataValueField = "Comp_Code";
+            DDLCompanyName.DataBind();
+            DDLCompanyName.Items.Insert(0, new ListItem("Select Company Name", "0"));
+        }
+    }
+
+
+
+    protected void fill_Finance_year()
+    {
+        DateTime dtm = new DateTime();
+        dtm = Convert.ToDateTime(DateTime.Now);
+        ArrayList Year = new ArrayList();
+      
+        Year.Add(Convert.ToString(dtm.Year - 3) + "-" + Convert.ToString(dtm.Year - 2));
+        Year.Add(Convert.ToString(dtm.Year-2) + "-" + Convert.ToString(dtm.Year-1));
+        Year.Add(Convert.ToString(dtm.Year-1) + "-" + Convert.ToString(dtm.Year));
+        Year.Add(Convert.ToString(dtm.Year) + "-" + Convert.ToString(dtm.Year + 1));
+        //Year.Add(Convert.ToString(dtm.Year + 1) + "-" + Convert.ToString(dtm.Year + 2));
+  
+        ddlFinanceYear.DataSource = Year;
+        ddlFinanceYear.DataBind();
+        ddlFinanceYear.Items.Insert(0, new ListItem("Select Finance Year", "0"));
+       
+    }
+
+
+    protected void btnSave_Click1(object sender, EventArgs e)
+    {
+        try
+        {
+            DataTable dtjv = new DataTable();
+           
+            ErrorContainer.Visible = true;
+
+            dtjv.Columns.Add("Comp_Code", typeof(string));
+            dtjv.Columns.Add("Note", typeof(string));
+            dtjv.Columns.Add("Items", typeof(string));
+            dtjv.Columns.Add("Qty", typeof(int));
+            dtjv.Columns.Add("Rate", typeof(decimal));
+            dtjv.Columns.Add("bs_Id", typeof(int));
+            DataRow dr = null;
+
+            DataTable dtjv2 = new DataTable();
+            dtjv2.Columns.Add("Comp_Code", typeof(string));
+            dtjv2.Columns.Add("Note", typeof(string));
+            dtjv2.Columns.Add("Items", typeof(string));
+            dtjv2.Columns.Add("Qty", typeof(int));
+            dtjv2.Columns.Add("Rate", typeof(decimal));
+            dtjv2.Columns.Add("bs_Id", typeof(int));
+
+            DataRow dr1 = null;
+          
+         
+         
+            foreach (GridViewRow gvr in GVBalance.Rows)
+            {
+              //  Label lblBS_ID = (Label)gvr.FindControl("lblBS_ID"); 
+                Label lblNote = (Label)gvr.FindControl("lblNote");
+                Label lblItem = (Label)gvr.FindControl("lblItem");
+                TextBox txtQTY = (TextBox)gvr.FindControl("txtQTY");
+                TextBox txtRATE = (TextBox)gvr.FindControl("txtRATE");
+
+
+                HiddenField hdnQTY = (HiddenField)gvr.FindControl("hdnQTY");
+                HiddenField hdnRATE = (HiddenField)gvr.FindControl("hdnRATE");
+
+                HiddenField bs_Id = (HiddenField)gvr.FindControl("hdnbsid");
+                HiddenField hdnAction = (HiddenField)gvr.FindControl("hdnAction");
+
+
+                if (txtQTY.Text == string.Empty || txtQTY.Text == "")                   
+                {
+                    txtQTY.Text = "0";
+                }
+                if (txtRATE.Text == string.Empty || txtRATE.Text == "")
+                {
+                    txtRATE.Text = "0";
+                }
+
+
+                if ((Convert.ToInt32(txtQTY.Text) != Convert.ToInt32(hdnQTY.Value) || Convert.ToDecimal(txtRATE.Text) != Convert.ToDecimal(hdnRATE.Value)) && Convert.ToInt32(bs_Id.Value) == Convert.ToInt32(  0))
+                {
+                    if ((Convert.ToInt32(txtQTY.Text) > 0) && (Convert.ToDecimal(txtRATE.Text) > 0))
+                    {
+
+                        dr = dtjv.NewRow();
+
+                        dr["Comp_Code"] = Convert.ToString(DDLCompanyName.SelectedValue);
+                        // dr["bs_Id"] = Convert.ToInt32(lblBS_ID.Text);
+                        dr["NOTE"] = Convert.ToString(lblNote.Text);
+                        dr["Items"] = Convert.ToString(lblItem.Text);
+                        dr["QTY"] = Convert.ToInt32(txtQTY.Text);
+                        dr["RATE"] = Convert.ToDecimal(txtRATE.Text);
+                        dr["bs_Id"] = Convert.ToInt32(bs_Id.Value);
+                        dtjv.Rows.Add(dr);
+                    }
+                }
+
+                else if ((Convert.ToInt32(txtQTY.Text) != Convert.ToInt32(hdnQTY.Value) ||  Convert.ToDecimal(txtRATE.Text) != Convert.ToDecimal(hdnRATE.Value)) && Convert.ToInt32(bs_Id.Value) >Convert.ToInt32(  0))
+                {
+                    if ((Convert.ToInt32(txtQTY.Text) > 0) && (Convert.ToDecimal(txtRATE.Text) > 0))
+                    {
+                        dr1 = dtjv2.NewRow();
+                        dr1["Comp_Code"] = Convert.ToString(DDLCompanyName.SelectedValue);
+                        // dr1["bs_Id"] = Convert.ToInt32(lblBS_ID.Text);
+                        dr1["NOTE"] = Convert.ToString(lblNote.Text);
+                        dr1["Items"] = Convert.ToString(lblItem.Text);
+                        dr1["QTY"] = Convert.ToInt32(txtQTY.Text);
+                        dr1["RATE"] = Convert.ToDecimal(txtRATE.Text);
+                        dr1["bs_Id"] = Convert.ToInt32(bs_Id.Value);
+                        dtjv2.Rows.Add(dr1);
+                    }
+
+                }
+
+
+                /*
+
+                if (bs_Id.Value == null || bs_Id.Value == "0")
+                {
+                   
+                    dr = dtjv.NewRow();
+
+                    if ((Convert.ToInt32(txtQTY.Text) > 0) && (Convert.ToDecimal(txtRATE.Text) > 0))
+                    {
+                        dr["Comp_Code"] = Convert.ToString(DDLCompanyName.SelectedValue);
+                       // dr["bs_Id"] = Convert.ToInt32(lblBS_ID.Text);
+                        dr["NOTE"] = Convert.ToString(lblNote.Text);
+                        dr["Items"] = Convert.ToString(lblItem.Text);
+                        dr["QTY"] = Convert.ToInt32(txtQTY.Text);
+                        dr["RATE"] = Convert.ToDecimal(txtRATE.Text);
+                        dr["bs_Id"] = Convert.ToInt32(bs_Id.Value);
+                        dtjv.Rows.Add(dr);
+                    }
+                }
+
+                else 
+                {
+
+                    dr1 = dtjv2.NewRow();
+                    dr1["Comp_Code"] = Convert.ToString(DDLCompanyName.SelectedValue);
+                   // dr1["bs_Id"] = Convert.ToInt32(lblBS_ID.Text);
+                    dr1["NOTE"] = Convert.ToString(lblNote.Text);
+                    dr1["Items"] = Convert.ToString(lblItem.Text);
+                    dr1["QTY"] = Convert.ToInt32(txtQTY.Text);
+                    dr1["RATE"] = Convert.ToDecimal(txtRATE.Text);
+                    dr1["bs_Id"] = Convert.ToInt32(bs_Id.Value);
+                    dtjv2.Rows.Add(dr1);
+                }
+         */
+                }
+
+                    string result = "";
+                    if (Convert.ToString(DDLCompanyName.SelectedValue) != "0" && (dtjv.Rows.Count > 0 || dtjv2.Rows.Count > 0))
+                    {
+
+                        result = Notes.Update_BS_NOTES(Convert.ToString(DDLCompanyName.SelectedValue), dtjv, dtjv2, Session["Employee_Code"].ToString());
+                        if (result == "")
+                        {
+                            //GVBalance.Visible = false;
+                            //DDLCompanyName.ClearSelection();
+                            //ddlFinanceYear.ClearSelection();
+
+
+                            msg.ShowMessage("Record Updation is Successfully done.", null, ErrorContainer, MyMessage, "Success");
+                        }
+
+                    
+                    //else 
+                    //{
+
+                    //    msg.ShowMessage("Record Updation Failed. Please Try Again.!", null, ErrorContainer, MyMessage, "Warning");
+                    //}
+
+                    }
+                    else
+                    {
+                        msg.ShowMessage("Please Fill Balance Sheet Details!", null, ErrorContainer, MyMessage, "Warning");
+
+                    }
+
+                    
+        }
+        catch (Exception ex)
+        {
+            ErrorContainer.Visible = true;
+            msg.ShowMessage(null, ex, ErrorContainer, MyMessage, null);
+        }
+        
+    }
+
+  
+    protected void fill_note()
+    {
+        DataTable dt = Notes.SELECT_BS_NOTES(ddlFinanceYear.SelectedValue, DDLCompanyName.SelectedValue); 
+        GVBalance.DataSource = dt;
+        GVBalance.DataBind();
+        ErrorContainer.Visible = false;
+        GVBalance.Visible = true;
+        btnSave.Visible = true;
+        gridheader.Visible = true;
+   
+    }
+
+
+
+    protected void ddlFinanceYear_SelectedIndexChanged(object sender, EventArgs e)
+    {
+         fill_note();
+      
+    }
+    protected void GVBalance_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        /*
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            HiddenField bs_Id = (HiddenField)e.Row.FindControl("hdnbsid");
+            HiddenField hdnAction = (HiddenField)e.Row.FindControl("hdnAction");
+            if (bs_Id.Value == null || bs_Id.Value == "0")
+            {
+                hdnAction.Value = "I";
+            }
+            else 
+            {
+                hdnAction.Value = "U";
+            }
+
+        }*/
+    }
+}
